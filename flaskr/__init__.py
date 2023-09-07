51fe6e9c8623d8b9cd9830b8e2529d28e51c90fe
OhMyDenta <dentaharvyndo@gmail.com>
2023-09-07 11:43:59 +0700

    add birthday

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,5 +1,12 @@
-#import aplikasi flask untuk di pakai di web
-from flask import Flask
+#import aplikasi flask , os, flash, jsonify, redirect, dan render_template untuk dipakai di web kita
+import os 
+#import SQL untuk akses database
+from cs50 import SQL
+#import flash untuk notifikasi pada web
+#import jsonify untuk mengformat data
+#import redirect dan render_template untuk berpidah halaman web
+#import request dan session untuk mengakses data
+from flask import Flask, flash , jsonify, redirect, render_template, request, session
 #mengatur nama aplikasi
 app = Flask(__name__)
 #mengatur uri (universal resource identifier), dan apa yang ditampilkan jika uri tersebut diakses 
